486749d3f7654072f9658b590be07c2f01a3a913
Chintu12a <52694889+Chintu12a@users.noreply.github.com>
2021-08-27 14:05:47 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,75 @@
+import React, { useState } from "react";
+import "./styles/index.scss";
+import questions from "./Components/Data";
+
+export default function App() {
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [showScore, setShowScore] = useState(false);
+  const [score, setScore] = useState(0);
+
+  const handleAnswerButton = (isCorrect) => {
+    if (isCorrect) {
+      setScore(score + 1);
+    }
+    const nextQuestion = currentQuestion + 1;
+    if (nextQuestion < questions.length) {
+      setCurrentQuestion(nextQuestion);
+    } else {
+      setShowScore(true);
+    }
+  };
+
+  return (
+    <>
+      <nav className="navbar shadow fixed-top navbar-expand-sm navbar-dark bg-primary">
+        <div className="container">
+          <img src="https://media-exp1.licdn.com/dms/image/C4E0BAQG4TVYYasY1oA/company-logo_100_100/0/1539053140310?e=1638403200&v=beta&t=flUdfWQYi-u9ua-1cWi1C7XkT-OHzpFeTeYZ5zRXpbE"></img>
+          <h2>Kalvi Fun Learning</h2>
+        </div>
+      </nav>
+      <div className="app">
+        {/* HINT: replace "false" with logic to display the 
+      score when the user has answered all the questions */}
+        {showScore ? (
+          <div className="score-section">
+            You scored {score} out of {questions.length}
+            <h4>
+              {score == questions.length ? (
+                <h6>"Wow! You're a Science wiz! Happy to have you here!"</h6>
+              ) : (
+                <h6>
+                  "You're mostly there! Let us help you love Science even more!
+                  "
+                </h6>
+              )}
+            </h4>
+          </div>
+        ) : (
+          <>
+            <div className="question-section">
+              <div className="question-count">
+                <span>Question {currentQuestion + 1} </span>/{questions.length}
+              </div>
+              <div className="question-text">
+                {questions[currentQuestion].questionText}
+              </div>
+            </div>
+            <div className="answer-section">
+              {questions[currentQuestion].answerOptions.map((answerOption) => {
+                return (
+                  <button
+                    onClick={() => {
+                      handleAnswerButton(answerOption.isCorrect);
+                    }}
+                  >
+                    {answerOption.answerText}
+                  </button>
+                );
+              })}
+            </div>
+          </>
+        )}
+      </div>
+    </>
+  );
+}
---
+++ src/Components/Data.js
@@ -0,0 +1,29 @@
+export default [
+  {
+    questionText: "The longest cell in human body is ",
+    answerOptions: [
+      { answerText: "Red blood cell", isCorrect: false },
+      { answerText: "Nerve cell", isCorrect: true },
+      { answerText: "Muscle cell", isCorrect: false },
+      { answerText: "Fat cell", isCorrect: false }
+    ]
+  },
+  {
+    questionText: "Which of the following is only present in plant cell?",
+    answerOptions: [
+      { answerText: "Cell membrane", isCorrect: false },
+      { answerText: "Cell Wall", isCorrect: true },
+      { answerText: "Ribosomes", isCorrect: false },
+      { answerText: "Lysosomes", isCorrect: false }
+    ]
+  },
+  {
+    questionText: "A prokaryotic cell has?",
+    answerOptions: [
+      { answerText: "Nucleus, cell membrane, cytoplasm", isCorrect: false },
+      { answerText: "Nucleus, mitochondria, cell membrane", isCorrect: false },
+      { answerText: "Nucleoid, cell membrane, ribosomes", isCorrect: true },
+      { answerText: "Nucleoid, cell wall, mitochondria", isCorrect: false }
+    ]
+  }
+];
